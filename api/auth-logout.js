3e594acfb715e808a36402d7ba51764e014a8bc7
thes0ftmachine <46765526+thes0ftmachine@users.noreply.github.com>
2026-08-07 13:41:40 -0700

    Delete api/auth-logout.js

--- api/auth-logout.js
+++
@@ -1,6 +0,0 @@
-import { clearCookie } from "./_lib/cookies.js";
-
-export default async function handler(req, res) {
-  clearCookie(res, "discogs_session");
-  return res.status(200).json({ ok: true });
-}
